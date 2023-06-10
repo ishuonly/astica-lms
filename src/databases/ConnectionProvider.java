5523f3380905c681505c31ef611c8129364be341
Ashi-00 <ashidubey0811@gmail.com>
2023-06-10 11:31:20 +0530

    Key_Generation_Form

--- src/databases/ConnectionProvider.java
+++ src/databases/ConnectionProvider.java
@@ -9,8 +9,8 @@ import java.sql.DriverManager;
 public class ConnectionProvider {
     public static Connection getConn(){
         try{
-           Class.forName("com.mysql.jdbc.Driver");
-           Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy?useSSL=false","root","code_ishag");
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/serverdb?zeroDateTimeBehavior=CONVERT_TO_NULL", "root", "dubey123");
            return con;
         }
         catch(Exception e){
